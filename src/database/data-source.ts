b22acd56288b2647a7b6fd1f022b6103644b2336
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:06:01 -0300

    feat: add database connection

---
+++ src/database/data-source.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: ["src/database/migrations/*.ts"]
+});
+
+AppDataSource.initialize();
