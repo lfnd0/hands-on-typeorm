2c62eaffa42027f88afba6fdba7fc36d06e4a359
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:11:28 -0300

    feat: add book entity

---
+++ src/entities/book.ts
@@ -0,0 +1,38 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid"
+
+import { Genre } from "./genre";
+
+@Entity("books")
+export class Book {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string
+
+  @Column()
+  duration: number
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @CreateDateColumn()
+  updated_at: Date
+
+  @Column()
+  genre_id: string
+
+  @ManyToOne(() => Genre)
+  @JoinColumn({ name: "genre_id" })
+  genre: Genre;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
