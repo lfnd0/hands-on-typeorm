769e4a35ec8a2475cb2545db268517ac619ec976
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:13:19 -0300

    feat: add create genre controller

---
+++ src/controllers/create-genre-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateGenreService } from "../services/create-genre-service";
+
+export class CreateGenreController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, description } = request.body;
+    const service = new CreateGenreService();
+    const result = await service.execute({ name, description });
+
+    if (result instanceof Error) {
+      return response.status(400).json({
+        message: result.message,
+        status: response.statusCode
+      });
+    }
+
+    return response.status(201).json(result);
+  }
+}
