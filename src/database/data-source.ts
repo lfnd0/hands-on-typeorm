b7ecb738ee1e053a03b61d0c991dead808599d0a
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:18:46 -0300

    feat: add entities

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -10,6 +10,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  entities: ["src/entities/*.ts"],
   migrations: ["src/database/migrations/*.ts"]
 });
 
