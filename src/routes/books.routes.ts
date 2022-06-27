82d5b5297fd99e114c1303f73ea90d2994818dfd
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-27 18:35:45 -0300

    feat: add list books route

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -5,5 +5,7 @@ import { ListBooksController } from "../controllers/list-books-controller";
 export const booksRouter = Router();
 
 const createBookController = new CreateBookController();
+const listBookController = new ListBooksController();
 
 booksRouter.post("/", createBookController.handle);
+booksRouter.get("/", listBookController.handle);
