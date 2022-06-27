d0dfca2ccd00743102475e9c0c0c1c9f424fb35d
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:20:09 -0300

    feat: add list genres route

--- src/routes/genres.routes.ts
+++ src/routes/genres.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import { CreateGenreController } from "../controllers/create-genre-controller";
+import { ListGenresController } from "../controllers/list-genres-controller";
 
 export const genresRouter = Router();
 
 const createGenreController = new CreateGenreController()
+const listGenresController = new ListGenresController()
 
 genresRouter.post("/", createGenreController.handle)
+genresRouter.get("/", listGenresController.handle)
