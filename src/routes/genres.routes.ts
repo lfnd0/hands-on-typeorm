57c54616161c1de1a995dd4a36d6bbc29579a585
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:22:57 -0300

    feat: add remove genre route

--- src/routes/genres.routes.ts
+++ src/routes/genres.routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 
 import { CreateGenreController } from "../controllers/create-genre-controller";
 import { ListGenresController } from "../controllers/list-genres-controller";
+import { RemoveGenreController } from "../controllers/remove-genre-controller";
 
 export const genresRouter = Router();
 
 const createGenreController = new CreateGenreController()
 const listGenresController = new ListGenresController()
+const removeGenreController = new RemoveGenreController()
 
 genresRouter.post("/", createGenreController.handle)
 genresRouter.get("/", listGenresController.handle)
+genresRouter.delete("/:id", removeGenreController.handle)
