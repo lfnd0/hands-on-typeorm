01393b6d43d7b100ef44dc35e32a1c89607a6cea
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-27 15:00:01 -0300

    feat: add alter genre route

--- src/routes/genres.routes.ts
+++ src/routes/genres.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AlterGenreController } from "../controllers/alter-genre-controller";
 
 import { CreateGenreController } from "../controllers/create-genre-controller";
 import { ListGenresController } from "../controllers/list-genres-controller";
@@ -6,10 +7,12 @@ import { RemoveGenreController } from "../controllers/remove-genre-controller";
 
 export const genresRouter = Router();
 
-const createGenreController = new CreateGenreController()
-const listGenresController = new ListGenresController()
-const removeGenreController = new RemoveGenreController()
+const createGenreController = new CreateGenreController();
+const listGenresController = new ListGenresController();
+const removeGenreController = new RemoveGenreController();
+const alterGenreController = new AlterGenreController();
 
-genresRouter.post("/", createGenreController.handle)
-genresRouter.get("/", listGenresController.handle)
-genresRouter.delete("/:id", removeGenreController.handle)
+genresRouter.post("/", createGenreController.handle);
+genresRouter.get("/", listGenresController.handle);
+genresRouter.delete("/:id", removeGenreController.handle);
+genresRouter.put("/:id", alterGenreController.handle);
