e6ebdf18041804fab280f1d7cc2023414d9d0e27
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-26 22:20:39 -0300

    feat: add remove genre service

---
+++ src/services/remove-genre-service.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../database/data-source";
+import { Genre } from "../entities/genre";
+
+export class RemoveGenreService {
+  async execute(id: string): Promise<void | Error> {
+    const genreRepository = AppDataSource.getRepository(Genre);
+
+    if (!(await genreRepository.findOne({ where: { id } }))) {
+      return new Error("Category does not exists");
+    }
+
+    await genreRepository.delete(id);
+  }
+}
