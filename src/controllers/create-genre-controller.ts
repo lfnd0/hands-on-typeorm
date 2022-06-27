ac0d8460294677105e496e4890a07fa710447ddb
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-27 14:44:55 -0300

    refactor: change create genre controller

--- src/controllers/create-genre-controller.ts
+++ src/controllers/create-genre-controller.ts
@@ -5,8 +5,8 @@ export class CreateGenreController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
     const service = new CreateGenreService();
-    const result = await service.execute({ name, description });
 
+    const result = await service.execute({ name, description });
     if (result instanceof Error) {
       return response.status(400).json({
         message: result.message,
