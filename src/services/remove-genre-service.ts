68410e368ef03799157bcdb31c414b94227456e2
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-27 11:40:01 -0300

    refactor: change remove genre service

--- src/services/remove-genre-service.ts
+++ src/services/remove-genre-service.ts
@@ -6,7 +6,7 @@ export class RemoveGenreService {
     const genreRepository = AppDataSource.getRepository(Genre);
 
     if (!(await genreRepository.findOne({ where: { id } }))) {
-      return new Error("Category does not exists");
+      return new Error("Genre does not exists");
     }
 
     await genreRepository.delete(id);
