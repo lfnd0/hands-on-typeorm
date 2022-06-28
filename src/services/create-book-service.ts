6543cfa09fc1e056743e0525541e21c8daf7d03c
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-28 00:03:53 -0300

    refactor: change create book service

--- src/services/create-book-service.ts
+++ src/services/create-book-service.ts
@@ -5,20 +5,25 @@ import { Genre } from "../entities/genre";
 interface IBookRequest {
   name: string,
   description: string,
+  writer: string,
   pages: number,
   genre_id: string
 }
 
 export class CreateBookService {
-  async execute({ name, description, pages, genre_id }: IBookRequest): Promise<Book | Error> {
+  async execute({ name, description, writer, pages, genre_id }: IBookRequest): Promise<Book | Error> {
     const genreRepository = AppDataSource.getRepository(Genre);
     if (!(await genreRepository.findOne({ where: { id: genre_id } }))) {
       return new Error("Genre does not exists");
     }
 
     const bookRepository = AppDataSource.getRepository(Book);
+    if (await bookRepository.findOne({ where: { name } })) {
+      return new Error("Book already exists");
+    }
+
     const book = bookRepository.create({
-      name, description, pages, genre_id
+      name, description, writer, pages, genre_id
     });
 
     await bookRepository.save(book);
